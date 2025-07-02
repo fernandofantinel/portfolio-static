a765359b9d2ee67e88ac47f4273c4d18d1b8a866
Fernando Fantinel <fantinel89@gmail.com>
2025-07-02 11:57:14 -0300

    refactor: remove unnecessary code

--- public/modal.js
+++ public/modal.js
@@ -1,20 +1,14 @@
 const modal = document.getElementById("modal");
-const thumbnails = document.querySelectorAll(".thumb");
 const modalImage = document.getElementById("modal-image");
-
-thumbnails.forEach((thumb) => {
-  thumb.addEventListener("click", () => {
-    const src = thumb.getAttribute("src");
-    modalImage.setAttribute("src", src);
-  
-    const alt = thumb.getAttribute("alt");
-    modalImage.setAttribute("alt", alt);
-  });
-});
 
 document.querySelectorAll(".openModal").forEach(function (element) {
   element.addEventListener("click", function () {
-    
+    const src = element.getAttribute("src");
+    modalImage.setAttribute("src", src);
+
+    const alt = element.getAttribute("alt");
+    modalImage.setAttribute("alt", alt);
+
     setTimeout(() => {
       modal.classList.remove("hidden");
     }, 300);
@@ -23,9 +17,11 @@ document.querySelectorAll(".openModal").forEach(function (element) {
 
 document.querySelectorAll(".closeModal").forEach(function (element) {
   element.addEventListener("click", function () {
+    modalImage.setAttribute("src", "");
+    modalImage.setAttribute("alt", "");
 
     setTimeout(() => {
       modal.classList.add("hidden");
     }, 300);
-    });
+  });
 });
